da0921d12b3e2f91ec2a49df20d9664c9a33baa0
quanglong <quang.long292@gmail.com>
2023-09-11 11:36:00 +0700

    Add service, observable/subscribe

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
@@ -12,8 +13,18 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm',
   };
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
